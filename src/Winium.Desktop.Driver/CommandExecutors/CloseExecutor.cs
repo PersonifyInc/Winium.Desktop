ac940d6ea1667914edb9fe3c9a60f8b4e82eafd9
quangnt <quang.nt@personifyinc.com>
2016-05-05 15:38:22 +0700

    Address comments: Move fixing for terminated process to Quit/CloseExecutor class instead of NewSessionExecutor class. Use new appoarch to find child processes instead of passing process name.

--- src/Winium.Desktop.Driver/CommandExecutors/CloseExecutor.cs
+++ src/Winium.Desktop.Driver/CommandExecutors/CloseExecutor.cs
@@ -1,5 +1,12 @@
 ﻿namespace Winium.Desktop.Driver.CommandExecutors
 {
+    #region using
+
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    #endregion
+
     internal class CloseExecutor : CommandExecutorBase
     {
         #region Methods
@@ -8,7 +15,26 @@
         {
             if (!this.Automator.ActualCapabilities.DebugConnectToRunningApp)
             {
-                if (!this.Automator.Application.Close())
+                // If application had exited, find and terminate all children processes
+                if (this.Automator.Application.HasExited())
+                {
+                    List<Process> children = new List<Process>();
+                    children = this.Automator.Application.GetChildPrecesses(this.Automator.Application.GetProcessId());
+                    if (children.Count == 0)
+                    {
+                        children = this.Automator.Application.GetAllPrecessesByName(this.Automator.Application.GetProcessName());
+                    }
+                    foreach (var child in children)
+                    {
+                        if (!child.HasExited && !this.Automator.Application.Close(child))
+                        {
+                            this.Automator.Application.Kill(child);
+                        }
+                    }
+                }
+
+                // If application is still running, terminate it as normal case
+                else if (!this.Automator.Application.Close())
                 {
                     this.Automator.Application.Kill();
                 }
